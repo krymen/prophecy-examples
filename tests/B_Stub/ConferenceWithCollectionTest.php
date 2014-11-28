3f984ee7e4ce19dde36ee1452fc408cb4e605d5f
Krzysztof Menżyk <krzysztof@menzyk.net>
2014-11-28 12:23:24 +0100

    Added different types of promises in ConferenceWithCollectionTest

--- tests/B_Stub/ConferenceWithCollectionTest.php
+++ tests/B_Stub/ConferenceWithCollectionTest.php
@@ -23,4 +23,36 @@ class ConferenceWithCollectionTest extends ProphecyTestCase
 
         $this->assertTrue($conference->has('first_talk'));
     }
+
+    /** @test */
+    public function it_adds_a_talk2()
+    {
+        $conference = new ConferenceWithCollection();
+
+        $talk = $this->prophesize(Talk::class);
+        $talk->name()->will(function () {
+            return 'first_talk';
+        });
+
+        $conference->addTalk($talk->reveal());
+
+        $this->assertTrue($conference->has('first_talk'));
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException \Exception
+     */
+    public function it_adds_a_talk3()
+    {
+        $conference = new ConferenceWithCollection();
+
+        $talk = $this->prophesize(Talk::class);
+        $talk->name()->willThrow(\Exception::class);
+
+        $conference->addTalk($talk->reveal());
+
+        $this->assertTrue($conference->has('first_talk'));
+    }
 }
